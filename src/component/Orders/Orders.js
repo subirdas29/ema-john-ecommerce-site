4d3d485678a9435a829674f7cbd178fd59baedf9
Subirdas29 <dasjoy104@gmail.com>
2022-10-09 00:34:11 +0600

    cart state added

--- src/component/Orders/Orders.js
+++ src/component/Orders/Orders.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
-    const products = useLoaderData()
+    const {products,initialCart} = useLoaderData() // return {products:products, initialCart : initialCart}
+    const [cart,setCart] = useState(initialCart);// Cart er remove and changing access user k diyar jonno useState use kora hoise.ar na hyle state use korar dorkar nai..
     return (
         <div>
-            <h2>This is Orders:{products.length}</h2>
+             <div className='shop-container'>
+            <div className="products-container">
+               
+                {
+                    
+                }
+
+            </div>
+            <div className="cart-container">
+               <Cart cart={cart}></Cart>
+               {/* added */}
+            </div>
+        </div>
         </div>
     );
 };
